4d441f712e45b0e663c7f16c862b49ec494e62de
Jins jk <70104058+JinsJK@users.noreply.github.com>
2025-07-15 16:21:48 +0200

    Add SPA fallback routing with custom nginx.conf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000
+  },
+  base: '/',
 })
